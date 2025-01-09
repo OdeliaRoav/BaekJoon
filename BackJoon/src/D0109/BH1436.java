1ba944330d1f3234266e0bdf223e65ef255ca1f0
pmsoo <pmsoo4@naver.com>
2025-01-09 14:45:34 +0900

    Initial

--- BackJoon/src/D0109/BH1436.java
+++ BackJoon/src/D0109/BH1436.java
@@ -1,10 +1,30 @@
 package D0109;
 
+import java.util.Scanner;
+
 public class BH1436 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int count = 0;
+		int number = 0;
+		
+		while(true) {
+			if(String.valueOf(number).contains("666")) {
+				count++;
+			}
+			if(count == N) {
+				System.out.println(number);
+				break;
+			}
+			
+			number++;
+		}
+		
+		
 	}
 
 }
