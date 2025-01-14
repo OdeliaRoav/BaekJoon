dd6770830e5382f2fab0fdcb8724df9f8ddca126
pmsoo <pmsoo4@naver.com>
2025-01-14 17:22:28 +0900

    Initial

--- BackJoon/src/D0114/B2869.java
+++ BackJoon/src/D0114/B2869.java
@@ -1,32 +1,56 @@
 package D0114;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class B2869 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		
-		int count = 0;
-		int num = 0;
-		
-		while(count <= c) {
-			
-			count += a;
-			num++;
-			count -= b;
-		
-			
-			
-		}
-		System.out.print(num);
-		
-		
-		
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        
+        int tmp1 = v - a;
+        int tmp2 = a - b;
+        
+        int day;
+        
+        day = (tmp1) / (tmp2) + 1;
+        
+        if ((tmp1) % (tmp2) != 0) day++;
+        
+
+        System.out.print(day);
+        
+        bw.flush(); // BufferedWriter 버퍼 비우기
+        bw.close(); // BufferedWriter 닫기
+    }
 }
+
+/*
+int a = Integer.parseInt(st.nextToken());
+int b = Integer.parseInt(st.nextToken());
+int c = Integer.parseInt(st.nextToken());
+
+int meter = 0;
+int day = 0;
+
+while(true) {
+	
+	meter = meter + a;
+	day++;
+	if(meter>=c) {
+		break;
+	}
+	meter = meter - b;
+}
+
+bw.write(day);*/
