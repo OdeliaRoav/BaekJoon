a09262d2aa85c72e9825a4fe0b7883d43c7515f5
pmsoo <pmsoo4@naver.com>
2025-01-20 16:35:38 +0900

    Initial

---
+++ BackJoon/src/D0120/AA2292.java
@@ -0,0 +1,28 @@
+package D0120;
+
+import java.util.Scanner;
+
+public class AA2292 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		int root = 1;
+		int range = 2;
+		
+		if(a == root) {
+			System.out.println(1);
+		}
+		else {
+			while(range<a) {
+				range = range + (root*6);
+				root++;
+			}
+			System.out.println(root);
+		}
+		
+	}
+
+}
---
+++ BackJoon/src/D0120/B1149.java
@@ -0,0 +1,12 @@
+package D0120;
+
+import java.util.Scanner;
+
+public class B1149 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
