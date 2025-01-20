10e761d865003fc25d8ce20b35709688104f37ec
pmsoo <pmsoo4@naver.com>
2025-01-20 15:13:59 +0900

    Initial

---
+++ BackJoon/src/D0120/A3040.java
@@ -0,0 +1,33 @@
+package D0120;
+
+import java.util.Scanner;
+
+public class A3040 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int Array[] = new int[9];
+		int sum = 0;
+		
+		for(int i = 0; i<9; i++) {
+			Array[i] = sc.nextInt();
+			sum = sum + Array[i];
+		}
+		
+		for(int i = 0; i<9; i++) {
+			for(int j = i+1; j<9; j++) {
+				if(sum - Array[i]- Array[j] == 100) {
+					for(int c =0; c<9; c++) {
+						if(c!=i && c!=j) {
+							System.out.println(Array[c]);
+						}
+					}
+				}
+			}
+		}
+		
+		
+	}
+
+}
