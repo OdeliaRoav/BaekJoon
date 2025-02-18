0787c6b1d4e7f9011e18c265fc291642962b8c46
pmsoo <pmsoo4@naver.com>
2025-02-18 17:35:46 +0900

    Initial

--- BackJoon/src/D0218/A18870.java
+++ BackJoon/src/D0218/A18870.java
@@ -1,47 +1,36 @@
 package D0218;
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class A18870 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int[] Array = new int[a];
+        int[] Array2 = new int[a]; // 좌표 압축 결과 저장
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		
-		int Array[] = new int[a];
-		int Array2[] = new int[a];
-		
-		//중복 시 생략은 안하는 듯.
-		
-		for(int i = 0; i<a; i++) {
-			 
-			Array[i]= sc.nextInt();
-			
-		}
-		
-		Arrays.sort(Array); //Array 배열 정렬
-			
-		for(int i = 0; i<a; i++) {
-			int count = 0;
-			int b = Array[i];
-			
-			for(int j = 0; j < a; j++) {
-				if(b > Array[j]) {
-					count++;
-				}
-				
-			}
-			Array2[i] = count;
-			
-		}
-		
-		for(int i = 0; i<a; i++) {
-			System.out.print(Array2[i] + " ");
-		}
-		
-		
-	}
+        // 입력 받기
+        for (int i = 0; i < a; i++) {
+            Array[i] = sc.nextInt();
+        }
 
+        // 좌표 압축 (O(N²) 방식)
+        for (int i = 0; i < a; i++) {
+            int count = 0; // 현재 숫자가 몇 번째 작은지 계산
+            for (int j = 0; j < a; j++) {
+                if (Array[j] < Array[i]) {
+                    count++; // 나보다 작은 값 개수 세기
+                }
+            }
+            Array2[i] = count; // 결과 저장
+        }
+
+        // 결과 출력
+        for (int i = 0; i < a; i++) {
+            System.out.print(Array2[i] + " ");
+        }
+        
+        sc.close();
+    }
 }
