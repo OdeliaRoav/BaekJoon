87a9459c4c876f8c90652fbb28c7376d5e051885
pmsoo <pmsoo4@naver.com>
2025-02-18 17:40:01 +0900

    Initial

--- BackJoon/src/D0218/A18870.java
+++ BackJoon/src/D0218/A18870.java
@@ -1,18 +1,23 @@
 package D0218;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class A18870 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
         
-        int a = sc.nextInt();
+        int a = Integer.parseInt(bf.readLine());
         int[] Array = new int[a];
         int[] Array2 = new int[a]; // 좌표 압축 결과 저장
 
         // 입력 받기
         for (int i = 0; i < a; i++) {
-            Array[i] = sc.nextInt();
+            Array[i] = Integer.parseInt(bf.readLine());
         }
 
         // 좌표 압축 (O(N²) 방식)
@@ -28,9 +33,9 @@ public class A18870 {
 
         // 결과 출력
         for (int i = 0; i < a; i++) {
-            System.out.print(Array2[i] + " ");
+        	bw.write(Array2[i]+ " ");
         }
-        
-        sc.close();
+        bw.flush();   //남아있는 데이터를 모두 출력시킴
+        bw.close();
     }
 }
