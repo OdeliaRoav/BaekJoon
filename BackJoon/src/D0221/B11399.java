d1581e7f5ca2051a795e15c83d21c9fc0810ae13
pmsoo <pmsoo4@naver.com>
2025-02-24 14:11:15 +0900

    Initial

--- BackJoon/src/D0221/B11399.java
+++ BackJoon/src/D0221/B11399.java
@@ -1,10 +1,19 @@
 package D0221;
 
+import java.util.Scanner;
+
 public class B11399 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(0);
+		
+		int Array[] = new int[a];
+		
+		int result = 0;
+		
+		
 	}
 
 }
