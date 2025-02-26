6a90aebebf7720cff97ca2072fedae1601a23353
pmsoo <pmsoo4@naver.com>
2025-02-26 16:48:48 +0900

    Initial

---
+++ BackJoon/src/D0226/B18185.java
@@ -0,0 +1,23 @@
+package D0226;
+
+import java.util.Scanner;
+
+public class B18185 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(); //라면 공장의 수
+		
+		int Array[] = new int[a];
+		
+		for(int i = 0; i < a; i++) {
+			Array[i] = sc.nextInt();
+		}
+		
+		
+		
+		
+	}
+
+}
