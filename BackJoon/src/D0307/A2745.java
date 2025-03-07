84416ee968cc44898adf7d2ab0f151fa315028c9
pmsoo <pmsoo4@naver.com>
2025-03-07 14:19:00 +0900

    Initial

---
+++ BackJoon/src/D0307/A2745.java
@@ -0,0 +1,17 @@
+package D0307;
+
+import java.util.Scanner;
+
+public class A2745 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		int b = sc.nextInt();
+		
+		System.out.println(Integer.parseInt(a,b));
+		
+	}
+
+}
