04be658d1514c3609de2af13197932f7dbfa4104
pmsoo <pmsoo4@naver.com>
2025-03-11 13:18:00 +0900

    Initial

---
+++ BackJoon/src/D0311/B9506.java
@@ -0,0 +1,17 @@
+package D0311;
+
+import java.util.Scanner;
+
+public class B9506 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+	}
+
+}
