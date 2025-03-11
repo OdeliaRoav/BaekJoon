48477e22220aa3f2171d0599ce7d987ce5a836d5
pmsoo <pmsoo4@naver.com>
2025-03-11 13:38:49 +0900

    Initial

--- BackJoon/src/D0311/B9506.java
+++ BackJoon/src/D0311/B9506.java
@@ -10,8 +10,31 @@ public class B9506 {
 		
 		
 		
-		
-		
+		for(int i = 0; i<100000; i++) {
+			int a = sc.nextInt();
+			int Array[] = new int[a];
+			int count = 0;
+			
+			if(a == -1) {
+				break;
+			}
+			else {
+				for(int j = 0; j<a; j++) {
+					if(a % (j+1) == 0) {
+						Array[count] = j+1;
+						count++;
+					}
+					
+				}
+				
+				
+				
+				
+				
+			}
+			
+		}
+	
 	}
 
 }
