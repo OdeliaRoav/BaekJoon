c2786bc5d264ccd92a933f5f44afdc00c32b6a2b
pmsoo <pmsoo4@naver.com>
2025-03-12 15:46:47 +0900

    Initial

--- BackJoon/src/D0311/B9506.java
+++ BackJoon/src/D0311/B9506.java
@@ -12,27 +12,22 @@ public class B9506 {
 			if(a == -1) {
 				break;
 			}
-			boolean d = true;
 			
 			int count = 0;
-			int count1 = 0;
-			int count2 = 0;
 			int sum = 0;
-			
 			int Array[] = new int[a];
+			boolean d = true;
+			
 			
 			for(int i = 0; i<a; i++) {
-				if(a % (i+1) == 0) {
+				if(a % (i+1) == 0 ) {
 					Array[count] = i+1;
 					count++;
-				}	
-				else if ( a == i ) {
-					continue;
 				}
 			}
 			
-			for(int i = 0; i<Array.length-1; i++) {
-				sum = sum + Array[i];
+			for(int i = 0; i<count-1; i++) {
+				sum += Array[i];
 			}
 			
 			if(sum == a) {
@@ -42,31 +37,14 @@ public class B9506 {
 				d = false;
 			}
 			
-		
-			if(d = true) {
-				for(int i = 0; i<Array.length; i++) {
-					if(Array[i] != 0) {
-						count1++;
-					}
-				}
-				
-				System.out.print(a + " = ");
-				for(int i = 0; i< count1-1; i++) {
-					System.out.print(Array[i] + " + ");
-					count2 ++ ;
-					
-					if(count2 == count1-2) {
-						break;
-					}
-				}
-				System.out.println(Array[count1-2]);
-			}
-			else {
+			if(d = false) {
 				System.out.println(a + " is NOT perfect.");
 			}
 			
+			
+			
+			
 		}
-		
 		
 	}
 
