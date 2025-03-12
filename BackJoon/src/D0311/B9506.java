4c44975bec4b86d38bee18395853374f2e2a013c
pmsoo <pmsoo4@naver.com>
2025-03-12 15:03:02 +0900

    Initial

--- BackJoon/src/D0311/B9506.java
+++ BackJoon/src/D0311/B9506.java
@@ -7,7 +7,21 @@ public class B9506 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
+		while(true) {
+			int a = sc.nextInt();
+			int count = 0;
+			
+			int Array[] = new int[a];
+			
+			for(int i = 0; i<a; i++) {
+				
+				
+				
+			}
+			
+			
+			
+		}
 		
 		
 		
