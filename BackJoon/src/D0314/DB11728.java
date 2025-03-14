48f1df5dd983966aebff08cc9ee0aed135a08d3b
pmsoo <pmsoo4@naver.com>
2025-03-14 16:10:23 +0900

    Initial

---
+++ BackJoon/src/D0314/DB11728.java
@@ -0,0 +1,48 @@
+package D0314;
+
+import java.util.Scanner;
+
+public class DB11728 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int[] Array = new int[a];
+        int[] Array2 = new int[b];
+        int[] result = new int[a + b];
+
+        
+        for (int i = 0; i < a; i++) {
+            Array[i] = sc.nextInt();
+        }
+        for (int i = 0; i < b; i++) {
+            Array2[i] = sc.nextInt();
+        }
+
+        
+        int i = 0, j = 0, k = 0;
+        while (i < a && j < b) {
+            if (Array[i] < Array2[j]) {
+                result[k++] = Array[i++];
+            } else {
+                result[k++] = Array2[j++];
+            }
+        }
+
+        
+        while (i < a) {
+            result[k++] = Array[i++];
+        }
+        while (j < b) {
+            result[k++] = Array2[j++];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
