58a4276f34c90fd43296808c915ee8a70e98f298
pmsoo <pmsoo4@naver.com>
2025-03-19 15:15:41 +0900

    Initial

---
+++ BackJoon/src/D0319/B1978.java
@@ -0,0 +1,29 @@
+package D0319;
+
+import java.util.Scanner;
+
+public class B1978 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		int Array[] = new int[a];
+		int count = 0;
+		
+		
+		for(int i = 0; i<Array.length; i++) {
+			int b = sc.nextInt();
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+}
