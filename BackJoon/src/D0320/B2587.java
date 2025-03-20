39f17adb1563120a837b0a9b855b669e6f694442
pmsoo <pmsoo4@naver.com>
2025-03-20 14:04:49 +0900

    Initial

--- BackJoon/src/D0320/B2587.java
+++ BackJoon/src/D0320/B2587.java
@@ -1,10 +1,30 @@
 package D0320;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class B2587 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int Array[] = new int[5];
+		int sum = 0;
+		
+		
+		for(int i = 0; i<5; i++) {
+			Array[i] = sc.nextInt();	
+			sum = sum + Array[i];
+		}
+		
+		Arrays.sort(Array);
+		
+		System.out.println(sum/5);
+		System.out.println(Array[2]);
+		
+		
+		
+		
 	}
 
 }
