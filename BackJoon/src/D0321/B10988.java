3d8ffe9b959351225e53a0736802eeb76c59212c
pmsoo <pmsoo4@naver.com>
2025-03-21 12:48:21 +0900

    Initial

---
+++ BackJoon/src/D0321/B10988.java
@@ -0,0 +1,31 @@
+package D0321;
+
+import java.util.Scanner;
+
+public class B10988 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		char Array[] = new char[a.length()];
+		int count = 0;
+		
+		
+		if(a.length()%2 == 0) {
+			System.out.print(0);
+		}
+		else {
+			for(int i = 0; i<a.length()/2; i++) {
+				Array[i] = a.charAt(i);
+			}
+			for(int i = a.length(); i>a.length()/2; i--) {
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
