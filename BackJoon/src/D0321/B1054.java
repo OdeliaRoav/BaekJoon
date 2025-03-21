4e4ed80cd6edddc71197ffc6f0d821ceeaf338c9
pmsoo <pmsoo4@naver.com>
2025-03-21 16:45:48 +0900

    Initial

---
+++ BackJoon/src/D0321/B1054.java
@@ -0,0 +1,16 @@
+package D0321;
+
+import java.util.Scanner;
+
+public class B1054 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+	}
+
+}
