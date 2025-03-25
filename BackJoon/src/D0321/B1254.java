51d13f11082699e6e1e998eda211d101d6e0b1a9
pmsoo <pmsoo4@naver.com>
2025-03-25 12:48:32 +0900

    Initial

--- BackJoon/src/D0321/B1254.java
+++ BackJoon/src/D0321/B1254.java
@@ -4,13 +4,26 @@ import java.util.Scanner;
 
 public class B1254 {
 
-	public static void main(String[] args)  {
-		Scanner sc = new Scanner(System.in);
-		
-		String s = sc.next();
-		int ans = 1;
-		
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int len = s.length();
+        int ans = 0;
 
+        for (int i = 0; i < len; i++) {
+            ans = 1; 
+
+            for (int j = 0; j < (len - i) / 2; j++) {
+                if (s.charAt(i + j) != s.charAt(len - 1 - j)) {
+                    ans = 0; 
+                    break;
+                }
+            }
+
+            if (ans == 1) {
+                System.out.println(len + i);
+                break;
+            }
+        }
+    }
 }
