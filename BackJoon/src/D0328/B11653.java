c6ef61e1ec0cd430eb520818a3aa55e5f7c0662d
pmsoo <pmsoo4@naver.com>
2025-03-28 17:32:35 +0900

    Initial

--- BackJoon/src/D0328/B11653.java
+++ BackJoon/src/D0328/B11653.java
@@ -8,23 +8,15 @@ public class B11653 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		ArrayList<Integer> List = new ArrayList<>();
-		
-	
 		int n = sc.nextInt();
 		
-		for(int i = 2; i<n; i++) {
-			if(n%i == 0) {
-				n/= i;
+		for(int i = 2; i<=n; i++) {
+			while(n % i == 0) {
+				System.out.println(i);
+				n /=i;
 			}
-			List.add(i);
+			
 		}
-		for(int i = 0; i<List.size(); i++) {
-			System.out.println(List.get(i));
-		}
-		
-		
-		
 	}
 
 }
