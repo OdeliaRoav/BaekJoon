07fe508eecef08518433669143f572e7b00c9d8a
pmsoo <pmsoo4@naver.com>
2025-03-28 17:22:13 +0900

    Initial

--- BackJoon/src/D0328/B2581.java
+++ BackJoon/src/D0328/B2581.java
@@ -4,17 +4,43 @@ import java.util.Scanner;
 
 public class B2581 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		int sum = 0;
-		
-		
-		
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = m; i <= n; i++) {
+            if (i < 2) continue; // 2보다 작은 수는 소수 아님
+
+            boolean isPrime = true;
+
+            // √i 까지만 나눠보면 충분
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                sum += i;
+                if (i < min) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+        sc.close();
+    }
 }
