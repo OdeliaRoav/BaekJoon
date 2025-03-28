7f9178284d15d603be85962b9f1c0f61cbb32dc9
pmsoo <pmsoo4@naver.com>
2025-03-28 14:34:40 +0900

    Initial

---
+++ BackJoon/src/D0328/B11650.java
@@ -0,0 +1,47 @@
+package D0328;
+import java.util.Scanner;
+
+class test{
+	int a;
+	int b;
+}
+
+public class B11650 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		test num [] =  new test[a];
+		
+		for(int i = 0; i<num.length; i++) {
+			num[i] = new test();
+		}
+		
+		for(int i = 0; i<a; i++) {
+			num[i].a = sc.nextInt();
+			num[i].b = sc.nextInt();
+		}
+	
+		
+		for (int i = 0; i < a - 1; i++) {
+            for (int j = 0; j < a - i - 1; j++) {
+                if (num[j].a > num[j + 1].a || 
+                   (num[j].a == num[j + 1].a && num[j].b > num[j + 1].b)) {
+
+                    test temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                }
+            }
+        }
+		
+		
+		for(int i = 0; i<a; i++) {
+			System.out.print(num[i].a + " " + num[i].b);
+		}
+		
+	
+	}
+}
