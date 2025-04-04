fd1e137e59f8b0374ac7bab112f8aa849bb0a8e3
pmsoo <pmsoo4@naver.com>
2025-04-04 16:25:08 +0900

    Initial

--- BackJoon/src/D0404/D3009.java
+++ BackJoon/src/D0404/D3009.java
@@ -7,9 +7,17 @@ public class D3009 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int x [] = new int[3];
+		int y [] = new int[3];
+
+		for(int i = 0; i<3; i++) {
+			x[i] = sc.nextInt();
+			y[i] = sc.nextInt();
+		}
 		
-		
-		
+		for(int i = 0; i<3; i++) {
+			System.out.println(x[i] + " " + y[i]);
+		}
 		
 	}
 
