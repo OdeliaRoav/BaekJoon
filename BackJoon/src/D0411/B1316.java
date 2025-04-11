9911885e30b5ef99fed6117c9c2e20a45a96b197
pmsoo <pmsoo4@naver.com>
2025-04-11 09:49:18 +0900

    Initial

--- BackJoon/src/D0411/B1316.java
+++ BackJoon/src/D0411/B1316.java
@@ -3,28 +3,32 @@ package D0411;
 import java.util.Scanner;
 
 public class B1316 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		
-		String b[] = new String[a];
-		
-		for(int i = 0; i < a; i++) {
-
-			b[i] = sc.next();
-
-		}
-		
+		int n = sc.nextInt();
 		int count = 0;
-		
-		for(int i = 0; i<a; i++) {
-			char c = b[i].charAt(0);
-			for(int j = 0; j<b.length; j++) {
-				
-			}
-		}
-	}
 
+		for(int i = 0; i < n; i++) {
+			String word = sc.next();
+			boolean[] check = new boolean[26];
+			boolean isGroup = true;
+
+			for(int j = 0; j < word.length(); j++) {
+				char now = word.charAt(j);
+
+				if(j > 0 && word.charAt(j) != word.charAt(j - 1)) {
+					if(check[now - 'a']) {
+						isGroup = false;
+						break;
+					}
+				}
+
+				check[now - 'a'] = true;
+			}
+
+			if(isGroup) count++;
+		}
+
+		System.out.println(count);
+	}
 }
