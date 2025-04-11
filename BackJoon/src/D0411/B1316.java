6eb8cbaf582b08dd2120f1d2e68e544f31c2a7be
pmsoo <pmsoo4@naver.com>
2025-04-11 09:31:50 +0900

    Initial

--- BackJoon/src/D0411/B1316.java
+++ BackJoon/src/D0411/B1316.java
@@ -7,8 +7,21 @@ public class B1316 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		int a = sc.nextInt();
 		
+		String b[] = new String[a];
 		
+		for(int i = 0; i < a; i++) {
+
+			b[i] = sc.next();
+
+		}
+		
+		for(int i = 0; i<a; i++) {
+			for(int j = 0; j<b.length; j++) {
+				
+			}
+		}
 	}
 
 }
