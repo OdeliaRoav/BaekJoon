a15f723a10afc80b70c5c2ef91ca5e2750fe8678
pmsoo <pmsoo4@naver.com>
2025-06-04 13:30:22 +0900

    Initial

--- BackJoon/src/D0404/D3009.java
+++ BackJoon/src/D0404/D3009.java
@@ -14,11 +14,7 @@ public class D3009 {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
         }
-     
-        
-   
-      
-        
+             
         
         int a, b;
 
